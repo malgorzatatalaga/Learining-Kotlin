6820d68b071e3355eef26935945a76728f8618f8
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-08 13:54:00 +0200

    8-06

--- src/main/kotlin/Hyperskill.kt
+++ src/main/kotlin/Hyperskill.kt
@@ -1,7 +1,8 @@
+import java.util.*
 import kotlin.math.abs
 
 fun main() {
-
+    knightsOnAChessboard(1, 1, 2, 3)
 }
 
 fun isVowel(letter: Char): Boolean {
@@ -11,4 +12,58 @@ fun isVowel(letter: Char): Boolean {
 
 fun getLastDigit(number: Int): Int {
     return abs(number % 10)
+}
+
+fun knightsOnAChessboard(x1: Int, y1: Int, x2: Int, y2: Int) {
+    if (abs(x1 - x2) == 1 && abs(y1 - y2) == 2) {
+        println("YES")
+    } else if (abs(x1 - x2) == 2 && abs(y1 - y2) == 1) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
+
+fun maximumElementDivisibleVy4() {
+    val scanner = Scanner(System.`in`)
+    var maximumNumber = 0
+    while (scanner.hasNext()) {
+        val number = scanner.nextInt()
+        if (number % 4 == 0 && number > maximumNumber) {
+            maximumNumber = number
+        }
+    }
+    println(maximumNumber)
+}
+
+fun theLengthOfTheSequence() {
+    val scanner = Scanner(System.`in`)
+    var sum = 0
+    while (scanner.hasNextInt()) {
+        val number = scanner.nextInt()
+        if (number > 0) {
+            sum ++
+        } else {
+            break
+        }
+    }
+    println(sum)
+}
+
+fun collatzConjecture(x: Int) {
+    print(x)
+    print(" ")
+    var number = x
+    while (number > 1) {
+        if (number % 2 == 0) {
+            number /= 2
+            print(number)
+            print(" ")
+        } else {
+            number *= 3
+            number++
+            println(number)
+            print(" ")
+        }
+    }
 }
--- src/main/kotlin/KotlinDocsNotes.kt
+++ src/main/kotlin/KotlinDocsNotes.kt
@@ -1,11 +1,9 @@
 fun main() {
 
-    //If when is used as an expression, the else branch is mandatory,
-    // unless the compiler can detect that all possible cases are covered by the branch conditions.
+//    If when is used as an expression, the else branch is mandatory,
+//    unless the compiler can detect that all possible cases are covered by the branch conditions.
 
-    val obj = "Hello"
-
-    val result = when (obj) {
+    val result = when ("Hello") {
         // If obj equals "1", sets result to "one"
         "1" -> "One"
         // If obj equals "Hello", sets result to "Greeting"
@@ -15,4 +13,42 @@ fun main() {
     }
     println(result)
     // Greeting
+
+//    In the example below, the if expression is passed directly to the println()
+//    function without declaring a variable, and println() prints the result.
+
+    val a = readLine()?.toInt()
+    val b = readLine()?.toInt()
+
+    println(
+        if (a == b) {
+            "a equal b"
+        } else if (a!! > b!!) {
+            "a is greater than b"
+        } else {
+            "a is less than b"
+        }
+    )
+
+//    when expressions can also be used with ranges and more complex conditions. Here's an example:
+    val number = 15
+
+    when {
+        number < 0 -> println("Negative number")
+        number in 1..10 -> println("Number between 1 and 10")
+        number % 2 == 0 -> println("Even number")
+        else -> println("Odd number greater than 10")
+    }
+
+//    Loops can be used to process characters, strings, and any other data types.
+//    The program below displays English letters in a single line.
+//    It is possible to get the next character (according to the Unicode table) using the
+//    increment operator.
+
+    var letter = 'A'
+
+    while (letter <= 'Z') {
+        print(letter)
+        letter++
+    }
 }
