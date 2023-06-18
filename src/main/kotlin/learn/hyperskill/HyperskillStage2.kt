f70e9d3219a76e467b4c67cf056a1a87832d1468
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-18 19:02:09 +0200

    hyperskill named, default and regular arguments

--- src/main/kotlin/learn/hyperskill/HyperskillStage2.kt
+++ src/main/kotlin/learn/hyperskill/HyperskillStage2.kt
@@ -1,7 +1,9 @@
 package learn.hyperskill
 
+import kotlin.math.hypot
+
 fun main() {
-    f(1.0)
+
 }
 
 fun FTNNumbers() {
@@ -38,6 +40,39 @@ fun f2(x: Double): Double {
     return 1 / (x * x)
 }
 
-fun f3(x: Double): Double  {
+fun f3(x: Double): Double {
     return x * x - 1
+}
+
+// Change and implement the perimeter() function to calculate the perimeter of a polygon
+// with 3 or 4 vertices, represented by X and Y coordinates. Vertices are passed sequentially.
+
+fun perimeter(
+    x1: Double,
+    y1: Double,
+    x2: Double,
+    y2: Double,
+    x3: Double,
+    y3: Double,
+    x4: Double = x1,
+    y4: Double = y1
+): Double {
+    return hypot(x1 - x2, y1 - y2) + hypot(x2 - x3, y2 - y3) + hypot(x3 - x4, y3 - y4)
+    + hypot(x4 - x1, y4 - x1)
+}
+
+fun carPrice(old: Int = 5, kilometers: Int = 100000, maximumSpeed: Int = 120, automatic: Boolean = false) {
+    val automaticBoolean: Int = if (automatic) {
+        1
+    } else {
+        0
+    }
+    val totalPrice = 20000
+    println(if (maximumSpeed > 120) {
+        totalPrice - old * 2000 + (maximumSpeed - 120) * 100 - kilometers/10000 * 200 + automaticBoolean * 1500
+    } else if (maximumSpeed < 120) {
+        totalPrice - old * 2000 - (120 - maximumSpeed) * 100 - kilometers/10000 * 200 + automaticBoolean * 1500
+    } else {
+        totalPrice - old * 2000 - kilometers/10000 * 200 + automaticBoolean * 1500
+    })
 }
