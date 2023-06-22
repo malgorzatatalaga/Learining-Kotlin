0ba851e0e419d1b3a54e708e15ef7d2c2cc57d16
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-22 17:37:04 +0200

    string arrays

--- src/main/kotlin/learn/documentation/Arrays.kt
+++ src/main/kotlin/learn/documentation/Arrays.kt
@@ -7,8 +7,8 @@ fun main() {
     val onlyMinusOne = IntArray(10).apply { fill(-1) }
     println("Only -1: ${onlyMinusOne.joinToString()}")
 
-    val numberS = IntArray(5) { readLine()!!.toInt() } // on each line single numbers from 1 to 5
-    println(numberS.joinToString())
+//    val numberS = IntArray(5) { readLine()!!.toInt() } // on each line single numbers from 1 to 5
+//    println(numberS.joinToString())
 
 //    comparing 2 arrays
     val numbers1 = intArrayOf(1, 2, 3, 4)
@@ -17,4 +17,13 @@ fun main() {
 
     println(numbers1.contentEquals(numbers2)) // true
     println(numbers1.contentEquals(numbers3)) // false
+
+//    string arrays
+    val stringArray = arrayOf("array", "of", "strings")
+    var newEmptyArray = emptyArray<String>()
+    newEmptyArray += "cats"
+    newEmptyArray += "are"
+    newEmptyArray += "cute"
+    println(newEmptyArray.joinToString())
+
 }
