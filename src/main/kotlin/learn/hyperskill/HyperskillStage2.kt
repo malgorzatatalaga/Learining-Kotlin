5109c2b875e61d7bb5626d8080ca4acf20101e71
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-22 18:27:01 +0200

    for loops and arrays

--- src/main/kotlin/learn/hyperskill/HyperskillStage2.kt
+++ src/main/kotlin/learn/hyperskill/HyperskillStage2.kt
@@ -1,5 +1,6 @@
 package learn.hyperskill
 
+import java.util.*
 import kotlin.math.hypot
 import kotlin.math.pow
 
@@ -10,8 +11,14 @@ fun main() {
         "amount" -> deposit(amount = number)
         "percent" -> deposit(percent = number)
         "years" -> deposit(years = number)
-
     }
+
+    val number1 = readLine()!!.toInt()
+    val number2 = readLine()!!.toInt()
+    val number3 = readLine()!!.toInt()
+    val output = (number1..number2).count { it % number3 == 0 }
+
+    println(output)
 }
 
 fun FTNNumbers() {
@@ -66,21 +73,66 @@ fun perimeter(
     y4: Double = y1
 ): Double {
     return hypot(x1 - x2, y1 - y2) + hypot(x2 - x3, y2 - y3) + hypot(x3 - x4, y3 - y4)
-    + hypot(x4 - x1, y4 - x1)
+    +hypot(x4 - x1, y4 - x1)
 }
 
 fun deposit(amount: Int = 1000, percent: Int = 5, years: Int = 10) {
     val finalAmount = (amount.toDouble() * (1.00 + percent / 100.00).pow(years.toDouble())).toInt()
     println(finalAmount)
 }
+
 class ByteTimer(var time: Int) {
     init {
         time = if (time > 127) {
             127
-        } else if (time < -128){
+        } else if (time < -128) {
             -128
         } else {
             time
         }
     }
 }
+
+fun counting() {
+    val nNumber = readLine()!!.toInt()
+    var numbers = emptyArray<Int>()
+    for (i in 1..nNumber) {
+        val number = readLine()!!.toInt()
+        numbers += number
+    }
+    val mNumber = readLine()!!.toInt()
+    val count = numbers.count { it == mNumber }
+    println(count)
+
+//    yes/no output
+    println(
+        if (numbers.contains(mNumber)) {
+            "YES"
+        } else {
+            "NO"
+        }
+    )
+}
+
+fun triples() {
+    val scanner = Scanner(System.`in`)
+    var triple = 0
+    val size = scanner.nextInt()
+    var numbers = arrayOf(size)
+
+    while (scanner.hasNext()) {
+        val number = scanner.nextInt()
+        numbers += number
+    }
+
+    numbers.forEach { i -> println(i) }
+
+    for (i in 0 until size - 2) {
+        if (numbers[i] == numbers[i + 1] - 1 && numbers[i] == numbers[i + 2] - 2) {
+            triple += 1
+        }
+    }
+
+    println(triple)
+}
+
