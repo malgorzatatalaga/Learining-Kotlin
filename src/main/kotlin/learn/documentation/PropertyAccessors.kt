97094d232607da16c4a5a70da1f398d433142d0d
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-22 21:36:11 +0200

    property accessors

---
+++ src/main/kotlin/learn/documentation/PropertyAccessors.kt
@@ -0,0 +1,18 @@
+package learn.documentation
+
+fun main() {
+    val smartOne = Smartphone("Ericsong")
+    smartOne.price = -24
+    val smartTwo = Smartphone("iNokhe")
+    println(smartTwo.price)
+    println(smartOne.price)
+    println(smartTwo.price - smartOne.price)
+
+}
+
+class Smartphone(val name: String) {
+    var price: Int = -5
+        get() {
+            return name.length - field
+        }
+}
