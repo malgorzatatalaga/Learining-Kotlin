bf086767df65c655d20835c4012b8c5cde070063
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-28 20:06:22 +0200

    processing strings

--- src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
+++ src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
@@ -15,6 +15,7 @@ fun main() {
     val line = readLine() ?: throw IllegalStateException()
     println("Elvis says: $line")
 
+    createAEuphoniousWord()
 }
 
 
@@ -50,12 +51,64 @@ fun compareCaseInsensitive() {
     if (s1 == s2) println(true) else println(false)
 }
 
+// getting substrings
 fun exchange() {
     val s1 = readLine()!!
     val first = s1.first()
     val last = s1.last()
     val s2 = s1.replace(last, first)
     println(s2.replaceFirst(first, last))
+}
+
+// processing strings
+fun theLuckyTicket() {
+    val number = readLine()!!
+    var list = number.split("")
+    // calculating the sum of the first three digits
+    var firstSum = 0
+    for (i in 1..3) {
+        val digit = list[i].toInt()
+        firstSum += digit
+    }
+
+    // calculating the sum of the last three digits
+    var secondSum = 0
+    for (i in list.size-2 downTo list.size-4) {
+        val digit = list[i].toInt()
+        secondSum += digit
+    }
+
+    if (firstSum ==  secondSum) println("Lucky") else println("Regular")
+}
+
+fun createAEuphoniousWord() {
+    /*
+    A word is considered euphonious if it doesn't have three or more vowels or consonants in a row.
+    Otherwise, it is considered discordant.
+    */
+    val word = readLine()!!
+    val vowels = listOf('a', 'o', 'e', 'i', 'u', 'y')
+    var vowelCount = 2
+    var consonantCount = 2
+    var result = 0
+    for (i in 0..word.length - 3) {
+        if (vowels.containsAll(listOf(word[i], word[i+1], word[i+2]))) vowelCount += 1
+        else if (!vowels.contains(word[i]) && !vowels.contains(word[i+1]) && !vowels.contains(word[i+2])) {
+            consonantCount += 1
+        } else {
+            vowelCount = 2
+            consonantCount = 2
+        }
+        if (consonantCount >= 3) {
+            result++
+            consonantCount = 1
+        }
+        if (vowelCount >= 3) {
+            result++
+            vowelCount = 1
+        }
+    }
+    println(result)
 }
 
 
