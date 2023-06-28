1ea5d4acf402e060564c02faa798e36c69c0f0db
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-06-28 18:13:00 +0200

    getting substrings

--- src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
+++ src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
@@ -1,20 +1,22 @@
 package learn.hyperskill
 
 fun main() {
-//  we can use it to get default values when the variable is null
+    // we can use it to get default values when the variable is null
     var x: String? = null
     x = x ?: "Hello"
     println(x) // "Hello"
 
     val age: Int? = null
-    val name: String? = "Bob"
+    val name: String = "Bob"
     val nickname: String? = null
     val length: Int = nickname?.length ?: 0
     println("$age ${name?.length} $length")
 
     val line = readLine() ?: throw IllegalStateException()
     println("Elvis says: $line")
+
 }
+
 
 fun makeAnException(): Nothing {
     throw Exception("I'm an exception!")
@@ -36,7 +38,24 @@ fun elvisOperator() {
     var name: String? = "Kotlin"
     val length1: Int? = name?.length
     print(length1 ?: 0)
-//  we can throw exceptions in the right part
+    // we can throw exceptions in the right part
     val length2: Int = name?.length
         ?: throw Exception("The name is null")
 }
+
+fun compareCaseInsensitive() {
+    val s1 = readLine()!!.toLowerCase()
+    val s2 = readLine()!!.toLowerCase()
+
+    if (s1 == s2) println(true) else println(false)
+}
+
+fun exchange() {
+    val s1 = readLine()!!
+    val first = s1.first()
+    val last = s1.last()
+    val s2 = s1.replace(last, first)
+    println(s2.replaceFirst(first, last))
+}
+
+
