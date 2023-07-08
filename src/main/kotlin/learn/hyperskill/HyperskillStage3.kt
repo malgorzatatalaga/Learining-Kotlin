98e7e71d4744e87af530e41eb462d6c51da7091e
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-07-08 12:47:47 +0200

    data class

--- src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
+++ src/main/kotlin/learn/hyperskill/HyperskillStage3.kt
@@ -4,7 +4,6 @@ fun main() {
 
 }
 
-
 fun makeAnException(): Nothing {
     throw Exception("I'm an exception!")
 }
@@ -138,5 +137,13 @@ open class Coffee(val cost: Int, val volume: Int)
 class Latte(val milk: String, cost: Int, volume: Int) : Coffee(cost, volume) {
     override fun toString(): String {
         return super.toString() + "; milk=$milk"
+    }
+}
+
+data class Box(val height: Int, val length: Int, val width: Int) {
+//  Modify the data class in order to print only the height, width, and size values.
+    var size: Int = height + length + width
+    override fun toString(): String {
+        return "Box(height=$height, width=$width, size=$size)"
     }
 }
