89889bae434bcd24c91966d8f97897536b45ea2e
Malgorzata Talaga <malgorzatatalaga32@gmail.com>
2023-07-14 14:14:03 +0200

    ansi codes

---
+++ src/main/kotlin/learn/hyperskill/AnsiCodes.kt
@@ -0,0 +1,24 @@
+package learn.hyperskill
+
+fun main() {
+    printRed()
+    printYellow()
+    printGreen()
+    printBlue()
+}
+
+fun printRed() {
+    println("red: \u001B[101m \u001B[0m")
+}
+
+fun printYellow() {
+    println("yellow: \u001B[103m \u001B[0m")
+}
+
+fun printGreen() {
+    println("green: \u001B[102m \u001B[0m")
+}
+
+fun printBlue() {
+    println("blue: \u001B[104m \u001B[0m")
+}
